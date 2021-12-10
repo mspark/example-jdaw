ca79dd33d956e4b355b332452675170a04913f2b
mspark <marcel-maze7@hotmail.de>
2021-12-10 20:04:13 +0100

    WIP

--- src/main/java/de/mspark/example/jdaw/guild/GuildRepoImpl.java
+++ src/main/java/de/mspark/example/jdaw/guild/GuildRepoImpl.java
@@ -1,37 +1,40 @@
 package de.mspark.example.jdaw.guild;
 
 import java.util.Optional;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
 
 import de.mspark.jdaw.guilds.GuildRepository;
 import de.mspark.jdaw.guilds.model.CustomGuildConf;
 
-@Component
+@Repository
+@Primary
 public class GuildRepoImpl implements GuildRepository {
 
+    private final ConcurrentLinkedQueue<CustomGuildConf> confs = new ConcurrentLinkedQueue<>();
+    
     @Override
     public CustomGuildConf save(CustomGuildConf g) {
-        // TODO Auto-generated method stub
-        return null;
+        confs.add(g);
+        return g;
     }
 
     @Override
     public void delete(CustomGuildConf entity) {
-        // TODO Auto-generated method stub
+        confs.remove(entity);
         
     }
 
     @Override
     public boolean exists(Long gid) {
-        // TODO Auto-generated method stub
-        return false;
+        return confs.stream().anyMatch(g -> g.id() == gid.longValue());
     }
 
     @Override
     public Optional<CustomGuildConf> findOne(Long gid) {
-        // TODO Auto-generated method stub
-        return null;
+        return confs.stream().filter(g -> g.id() == gid.longValue()).findAny();
     }
     
 }
---
+++ src/main/java/de/mspark/example/jdaw/guild/SetOwnPrefix.java
@@ -0,0 +1,46 @@
+package de.mspark.example.jdaw.guild;
+
+import java.util.List;
+import java.util.Optional;
+
+import de.mspark.jdaw.Command;
+import de.mspark.jdaw.CommandProperties;
+import de.mspark.jdaw.JDAManager;
+import de.mspark.jdaw.config.JDAWConfig;
+import de.mspark.jdaw.guilds.GuildConfigService;
+import de.mspark.jdaw.guilds.GuildRepository;
+import de.mspark.jdaw.guilds.model.CustomGuildConf;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+@CommandProperties(trigger = "prefix", description = "Sets the prefix", userGuildPermissions = Permission.MANAGE_SERVER, helpPage = false)
+public class SetOwnPrefix extends Command {
+    private GuildRepository repo;
+    
+    public SetOwnPrefix(JDAWConfig config, GuildConfigService guildConfig, GuildRepository repo, JDAManager jdas) {
+        super(config, guildConfig, jdas, true);
+        this.repo = repo;
+    }
+
+    @Override
+    public void doActionOnCmd(Message msg, List<String> cmdArguments) {
+        String prefix = filter(cmdArguments.get(0)).get();
+        var guildConfig = repo.findOne(msg.getGuild().getIdLong())
+            .map(g -> new CustomGuildConf(g.id(), prefix, g.whitelist()))
+            .orElse(new CustomGuildConf(msg.getGuild().getIdLong(), prefix));
+        repo.save(guildConfig);
+        msg.reply("ok").submit();
+    }
+
+    @Override
+    protected MessageEmbed fullHelpPage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    // !!! SQL Injection possible - just for example !!!
+    public Optional<String> filter(String desiredPrefix) {
+        return Optional.of(desiredPrefix);
+    }
+}
